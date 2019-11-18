748419915a37e68e2791e65521a6916377c2da88
jialei zhou <47931522+AAlexZhou@users.noreply.github.com>
2019-11-17 19:32:20 -0600

    Add files via upload

---
+++ lab6/test.cpp
@@ -0,0 +1,101 @@
+#include "test.h"
+void test::test1()
+{
+  Queue test;
+  if (test.isEmpty())
+  {
+    std::cout << "Test 1: Newly created Queue is empty: PASSED" << '\n';
+  }
+  else
+  {
+    std::cout << "Test 1: Newly created Queue is empty: FAILED" << '\n';
+  }
+}
+void test::test2()
+{
+  Queue test;
+  test.enqueue(1);
+  if (!test.isEmpty())
+  {
+    std::cout << "Test 2: Enqueing a value makes the Queue not empty: PASSED" << '\n';
+    test.dequeue();
+    if (test.isEmpty())
+    {
+      std::cout << "Test 3: dequeing a value make the Queue empty: PASSED" << '\n';
+    }
+    else
+    {
+      std::cout << "Test 3: dequeing a value make the Queue empty: FAILED" << '\n';
+    }
+  }
+  else
+  {
+    std::cout << "Test 2: Enqueing a value makes the Queue not empty: FAILED" << '\n';
+  }
+}
+void test::test3()
+{
+  Queue test;
+  try
+  {
+    test.dequeue();
+    std::cout << "TEST 4:dequeing the empty queue: FAILED" << '\n';
+  }
+  catch(std::runtime_error & t)
+  {
+    std::cout << "TEST 4:dequeing the empty queue: PASSED" << '\n';
+  }
+}
+void test::test4()
+{
+  Queue test;
+  try
+  {
+    test.peekFront();
+    std::cout << "TEST 5:peek the empty queue: FAILED" << '\n';
+  }
+  catch(std::runtime_error & t)
+  {
+    std::cout << "TEST 5:peek the empty queue: PASSED" << '\n';
+  }
+  test.enqueue(1);
+  if (test.peekFront()==1)
+  {
+    std::cout << "TEST 6:peek a Queue with one number: PASSED" << '\n';
+  }
+  else
+  {
+    std::cout << "TEST 6:peek a Queue with one number: FAILED" << '\n';
+  }
+}
+void test::test5()
+{
+  Queue test;
+  test.enqueue(1);
+  test.enqueue(2);
+  if (test.peekFront()==1)
+  {
+    std::cout << "Test 7: Enqueing values in order: PASSED" << '\n';
+    test.dequeue();
+    if (test.peekFront()==2)
+    {
+      std::cout << "Test 8: Dequeing values in order: PASSED" << '\n';
+    }
+    else
+    {
+      std::cout << "Test 8: Dequeing values in order: FAILED" << '\n';
+    }
+  }
+  else
+  {
+    std::cout << "Test 7: Enqueing values in order: FAILED" << '\n';
+  }
+}
+void test::run()
+{
+  test1();
+  test2();
+  test3();
+  test4();
+  test5();
+}
---
+++ lab6/test.h
@@ -0,0 +1,17 @@
+#ifndef TEST_H
+#define  TEST_H
+#include <iostream>
+#include <stdexcept>
+#include "Queue.h"
+class test
+{
+public:
+  void run();
+private:
+  void test1();
+  void test2();
+  void test3();
+  void test4();
+  void test5();
+};
+#endif
